f9edba1b981eee98eba2124089a3cf564a94554b
woonny <kjw8739@naver.com>
2021-02-24 21:35:34 +0900

    2021 02 24 git clone 반응형 및 완성

---
+++ github clone/main.js
@@ -0,0 +1,30 @@
+(function(window, document){
+    'use strict';
+
+    const $toggles = document.querySelectorAll('.toggle');
+    const $toggleBtn = document.getElementById('toggle-btn'); // Return Element
+  
+  $toggleBtn.addEventListener('click', function () {
+    toggleElements();
+  });
+
+  window.addEventListener('resize', function () {
+    if (window.innerWidth > 1024) {
+      // Off toggle element
+      offElements();
+    }
+  });
+
+  function toggleElements() {
+    [].forEach.call($toggles, function (toggle) {
+      toggle.classList.toggle('on');
+    });
+  }
+
+  function offElements() {
+    [].forEach.call($toggles, function (toggle) {
+      toggle.classList.remove('on');
+    });
+  }
+
+})(window, document)
